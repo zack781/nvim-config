d0fe5fda58961bde5466fd1ae0dd1dfb4856bfad
zacknguyen <thean781@gmail.com>
2026-05-13 15:28:35 -0400

    added line number

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.number = true
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
